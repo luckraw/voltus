51548e3b41f01715bf55eca6ee459325b84c1d50
Lucas <krawdev@gmail.com>
2024-10-10 12:31:17 -0300

    feat: first commit

---
+++ src/main/java/com/luckraw/voltus/VoltusApplication.java
@@ -0,0 +1,13 @@
+package com.luckraw.voltus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoltusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckraw/voltus/VoltusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckraw.voltus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoltusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
